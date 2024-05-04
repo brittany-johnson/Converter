43aa1fd9b5a8cb1e5c5c37da41ca4197674a414f
brittany-johnson <brittanyjohnson.dev@gmail.com>
2024-05-03 20:53:43 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Brittany Johnson on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
